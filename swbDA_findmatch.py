8150af7a233fcdc2fd87ed11318d8af7432de1ef
Yang Xu <yvx5085@dreitter-imac.ist.psu.edu>
2015-10-07 11:24:44 -0400

    add the similarity_score function

--- swbDA_findmatch.py
+++ swbDA_findmatch.py
@@ -4,15 +4,17 @@
 # Yang Xu
 # 10/6/2015
 
-import re
+import re, math
 
 
 def fetch_text(file_name, num_line = 10):
     """
     Extract the pure text (no punctuations) of the first num_line lines of the file_name
+    return them in a list of words
     """
     count = 1
     start_count = False
+    results = []
     with open(file_name, 'rb') as fr:
         for line in fr:
             # if start_count is set to True, start processing
@@ -27,17 +29,36 @@ def fetch_text(file_name, num_line = 10):
                     except Exception, e:
                         print items
                         raise e
-                    print text.strip()
-                    count += 1
+                    # get the words
+                    words = text.split()
+                    words = [w for w in words if w != '']
+                    results += words
             # check if '=====' is reached
             if line.startswith('====='):
                 start_count = True
             # check if num_line is reached
             if count > num_line:
                 break
+    # return
+    return results
+
+
+# A simple function that computes the similarity between two chuncks of text
+def similarity_score(text1, text2):
+    """
+    text1 and text2 are lists of words
+    the score returned is the ratio of the interection of the two sets over the product of the two length
+    return: score = (set1 & set2) / sqrt(len(set1) * len(set2))
+    """
+    set1 = set(text1)
+    set2 = set(text2)
+    score = len(set1 & set2) / math.sqrt(len(set1) * len(set2))
+    return score
+
 
 # main
 if __name__ == '__main__':
     # exp
     file_name = 'swb1_dialogact_annot/sw00utt/sw_0001_4325.utt'
-    fetch_text(file_name, num_line = 10)
+    text = fetch_text(file_name, num_line = 10)
+    print similarity_score(text, text)
