a64063adea6a6a1a4f8f8e81e1174d5c8ed7ca26
Yang Xu <innerfirexy@gmail.com>
2015-10-14 15:46:30 -0400

    complete main function

--- DA_addtags2db.py
+++ DA_addtags2db.py
@@ -3,7 +3,7 @@
 # Yang Xu
 # 10/14/2015
 
-import MySQLdb, re
+import MySQLdb, re, sys
 
 
 # read all tags from a .utt file
@@ -53,4 +53,28 @@ if __name__ == '__main__':
         for line in fr:
             match.append(tuple(line.strip().split(',')))
 
-    # 
+    # db init: ssh yvx5085@brain.ist.psu.edu -i ~/.ssh/id_rsa -L 1234:localhost:3306
+    conn = MySQLdb.connect(host = "127.0.0.1", 
+                    user = "yang", 
+                    port = 3306,
+                    passwd = "05012014",
+                    db = "swbd")
+    cur = conn.cursor()
+
+    # for each pair in match, read the tags, 
+    # and then add to the correponding sentence in the entropy table
+    for i, pair in enumerate(match):
+        fn = pair[0]
+        cid = pair[1]
+        tag_res = read_tags(fn)
+        for item in tag_res:
+            tag = item[0]
+            sp = item[1]
+            tid = item[2]
+            loc = item[3]
+            sql = 'UPDATE entropy SET WS97 = %s WHERE convID = %s AND turnID = %s AND speaker = %s AND localID = %s'
+            cur.execute(sql, (tag, cid, tid, sp, loc))
+        sys.stdout.write('\r%d/%d updated' % (i+1, len(match)))
+        sys.stdout.flush()
+    # commit
+    conn.commit()
